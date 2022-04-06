e941bed48040f4ccc0b26241e1e7d65d92003ce7
Mary Priyanka <s160193@rguktsklm.ac.in>
2022-04-06 19:25:59 +0530

    new folder

---
+++ OMS/src/main/java/com/rakuten/Order.java
@@ -0,0 +1,19 @@
+package com.rakuten;
+
+public class Order {
+	int price;
+	String Product;
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getProduct() {
+		return Product;
+	}
+	public void setProduct(String product) {
+		Product = product;
+	}
+
+}
