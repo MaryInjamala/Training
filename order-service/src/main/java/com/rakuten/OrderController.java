adc30a8dc7cb14bbac5337c6ebb3507fd0eb9426
Mary Priyanka <86511097+MaryPriyankaI@users.noreply.github.com>
2022-04-20 23:06:48 +0530

    order service

---
+++ order-service/src/main/java/com/rakuten/OrderController.java
@@ -0,0 +1,19 @@
+package com.rakuten;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+	@Autowired
+	OrderService orderService;
+	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void CreateOrder(@RequestBody Order order) {
+		orderService.saveOrder(order);
+	}
+}
